1904e2ed82217810dabde7ee4ca3c95310d7b27d
jocelyn <jocelynwlt2026@student.cis.edu>
2024-03-14 14:26:50 +0800

    finish

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,37 @@
-public class Song {
+import java.util.ArrayList;
+import java.util.List;
 
-    private String
+public class Song {
+    private String title;
+    private String artist;
+    private List<String> listeners;
+
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+        this.listeners = new ArrayList<>();
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+    public int howMany(List<String> newlisteners){
+        int count = 0;
+        for(String listener: newlisteners){
+            String lclistener = listener.toLowerCase();
+            if(!listeners.contains(lclistener)){
+                listeners.add(lclistener);
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
 }
